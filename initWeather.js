36c1f797394d628f081344e40d4150816003bef8
Pawel-Mielniczuk <mrjeronimo@onet.pl>
2019-08-11 16:53:43 +0200

    add showVisibility function

--- initWeather.js
+++ initWeather.js
@@ -1,9 +1,12 @@
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    console.log('dziala')
+// const svg = document.querySelector('.circle');
+// svg.setAttribute('stroke-dasharray', '50, 100');
+
+if ('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    console.log('dziala');
   });
 } else {
-  console.log('nie dziala')
+  console.log('nie dziala');
 }
 
 function renderInitMessage() {
@@ -12,10 +15,9 @@ function renderInitMessage() {
   cityElement.textContent = message;
 }
 
-
 async function geoSuccess(position) {
-  const { latitude:lat, longitude:lon } = position.coords;
-  
+  const { latitude: lat, longitude: lon } = position.coords;
+
   const data = await getInitWeather(lat, lon);
   renderInitMessage();
   showInitWeather(data);
@@ -24,33 +26,42 @@ async function geoSuccess(position) {
 function geoError(errorObj) {
   let errorMessage = '';
 
-  switch(errorObj.code) {
-    case errorObj.PERMISSION_DENIED :
+  switch (errorObj.code) {
+    case errorObj.PERMISSION_DENIED:
       errorMessage = 'Brak pozwolenia na znalezienie lokalicazji';
       break;
-    case errorObj.POSITION.UNAVAILABLE :
-    errorMessage = 'Brak dostepu do sieci';
-    break;
+    case errorObj.POSITION.UNAVAILABLE:
+      errorMessage = 'Brak dostepu do sieci';
+      break;
 
-    case errorObj.TIMEOUT :
+    case errorObj.TIMEOUT:
       errorMessage = 'Przekroczono czas oczekiwania';
       break;
   }
-};
+}
 
+/**
+ * 
+ * 
+ */
+async function showVisibility(weatherData) {
+  document
+    .querySelector('.circle')
+    .setAttribute('stroke-dasharray', `${weatherData}, 100`);
+  document.querySelector('.percentage').textContent = `${weatherData.toFixed(1)}km`;
+};
 
 
 async function getInitWeather(lat, lon) {
   const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${apiKey}/${lat},${lon}?units=si`;
   const response = await fetch(url);
-  const data =  await response.json();
-  
-  return data;
-};
+  const data = await response.json();
 
+  return data;
+}
 
 async function showInitWeather(weatherData) {
-  
+  const currentWeather = weatherData.currently;
   const hourlyWeather = weatherData.hourly.data; //?
   const dailyWeather = weatherData.daily.data; // ?
   const nextSevenHoursWeather = await nextHours(hourlyWeather);
@@ -63,12 +74,13 @@ async function showInitWeather(weatherData) {
   renderDayOfMonth(dailyWeather);
   renderHourlyIconIntoHTML(hourlyWeather);
   renderDailyIconIntoHTML(dailyWeather);
-};
+  showVisibility(currentWeather.visibility);
+}
 
-
-
-
-window.addEventListener('DOMContentLoaded', () => {
-  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-}, false);
-
+window.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  },
+  false
+);
